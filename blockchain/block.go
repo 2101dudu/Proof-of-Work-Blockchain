1504fcd962efa5c0523670204680d7699f18f31b
Eduardo Faria <eduardo21faria@gmail.com>
2025-03-09 19:17:29 +0000

    feat: prepare utility functions

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,6 +1,8 @@
 package blockchain
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"log"
 	"strconv"
@@ -33,4 +35,26 @@ func createBlock(data []byte, prevHash []byte) *Block {
 // create a genesis block exists — without it, the first "real" block would now have a previous block hash to reference
 func genesis() *Block {
 	return createBlock([]byte("GENSIS_BLOCK"), []byte{})
+}
+
+// GO's BadgerDB requires byte slices, so a Serialize() needs to exist
+func (b *Block) Serialize() []byte {
+	var buf bytes.Buffer
+	enconder := gob.NewEncoder(&buf)
+	err := enconder.Encode(b)
+
+	Handle(err)
+
+	return buf.Bytes()
+}
+
+// GO's BadgerDB requires byte slices, so a Deserialize() needs to exist
+func Deserialize(data []byte) (b *Block) {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	err := decoder.Decode(&block)
+
+	Handle(err)
+
+	return &block
 }
--- blockchain/utils.go
+++ blockchain/utils.go
@@ -10,9 +10,15 @@ import (
 func toHex(num int64) []byte {
 	buff := new(bytes.Buffer)
 	err := binary.Write(buff, binary.BigEndian, num)
+
+	Handle(err)
+
+	return buff.Bytes()
+}
+
+// helper error function
+func Handle(err error) {
 	if err != nil {
 		log.Panic(err)
 	}
-
-	return buff.Bytes()
 }
